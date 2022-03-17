e46b5b4070de790bdd5dedba36e1d10e7996257a
SabreOfWinter <steadmanbailey@gmail.com>
2022-03-17 19:21:43 +0000

    Completed lesson 1-1

---
+++ 1-Intro/1-1-First-Program/main.cpp
@@ -0,0 +1,17 @@
+/*
+    Headers
+    Built-in library for Input/Output operations
+*/
+#include <iostream>
+
+/*
+    Main funtion, (AKA Entry Point)
+    The first block of code run in C++ program
+*/
+int main(){
+    std::cout << "Hello world" << std::endl; //endl pushs the cursor to the next line
+    std::cout << "1 : ";
+    std::cout << "2";
+
+    return 0; //Tells the Operating System whether the function was successful/failure in execution
+}
